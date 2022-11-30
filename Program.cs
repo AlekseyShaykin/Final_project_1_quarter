23919031748d4f75a3b43e6478dad3c028e754a1
AlekseyShaykin <alekseyshaykin2311@gmail.com>
2022-11-30 14:16:14 +0700

    I've adjusted the code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿// Написать программу, которая из имеющегося массива строк, длина которых меньше, либо равна 3 символа. 
+﻿// Написать программу, которая из имеюgщегося массива строк, длина которых меньше, либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
 
-string[] array = { "YES", "12#4fg", "Man", "Novosibirsk" };
+string[] array = { "YES", "12#4fg", "Man", "2", "Hello", "Red", "Oop" };
 
 void PrintArray(string[] arr)
 {
@@ -17,18 +17,29 @@ void PrintArray(string[] arr)
 }
 
 
-void Method(string[] arr)
+string[] Method(string[] arr)
 {
-    Console.Write(" --> [ ");
+    Console.Write(" -->");
+    int size = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+        int word = arr[j].Length;
+        if (word <= 3) size++;
+    }
+
+    string[] array = new string[size];
+    int index = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        int a = new int();
-        a = arr[i].Length;
-        if (a <= 3) Console.Write($"{arr[i]} ");
+        int word = arr[i].Length;
+        if (word <= 3) array[index++] = arr[i];
     }
-    Console.Write("]");
+    return array;
 }
 
 
+
 PrintArray(array);
-Method(array);
+string[] array1 = Method(array);
+PrintArray(array1);
+
